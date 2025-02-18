6880e98a4f1d3f7e11c5f98777405a229467158c
ashthejuan <akshitdg.7564@gmail.com>
2025-02-18 21:47:47 +0530

    css fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,6 @@ const Header = ({toggleTheme, isDarkMode}) => {
     return (
         <header className={`header ${isDarkMode ? "dark" : "light"}`}>
             <h1>Currency Converter</h1>
-            {/* <button className="theme-toggle" onClick={toggleTheme}>
-                {isDarkMode ? "Light Mode" : "Dark Mode"}
-            </button>         */}
             <label className="theme-switch">
                 <input type="checkbox" checked={isDarkMode} onChange={toggleTheme} />
                 <span className="slider"></span>
