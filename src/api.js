e3add639c8da2d06897961c36793935ab43985c4
ashthejuan <akshitdg.7564@gmail.com>
2025-02-19 10:21:41 +0530

    added .gitignore

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-const API_KEY = '6a2e51c3dcab5ecf202e3aad'
-const BASE_URL = 'https://v6.exchangerate-api.com/v6'
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = 'https://v6.exchangerate-api.com/v6';
 
 export const fetchExchangeRate = async (baseCurrency) => {
     try {
         const response = await axios.get(`${BASE_URL}/${API_KEY}/latest/${baseCurrency}`);
+        console.log('API Response:', response.data);
         return response.data.conversion_rates;
     } catch (error) {
         console.error('Error fetching exchange rate', error);
